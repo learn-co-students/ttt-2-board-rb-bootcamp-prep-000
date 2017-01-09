282ff11f757f52b556ebe2c4b7a33020a5b91158
David Mieloch <davidmieloch@gmail.com>
2017-01-09 23:43:42 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,7 @@
 # Define the variable board below.
+# 1. Define a local variable in `lib/board.rb` called `board`
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# 2. Set the `board` local variable equal to an array.
+# 3. Fill the `board` array with 9 strings containing a single space: `" "`.
+# 4. Run `learn` and read output and fix any errors.
+# 5. Submit your solution with `learn submit`.
