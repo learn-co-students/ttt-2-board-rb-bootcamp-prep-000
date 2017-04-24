a7cbb02d3548ab8ae294fb1d70e998132ce7c9c6
Richard Steinbach <steinbach.dick@gmail.com>
2017-04-24 04:17:02 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,11 @@
 # Define the variable board below.
+board = []
+board[0] = " "
+board[1] = " "
+board[2] = " "
+board[3] = " "
+board[4] = " "
+board[5] = " "
+board[6] = " "
+board[7] = " "
+board[8] = " "
