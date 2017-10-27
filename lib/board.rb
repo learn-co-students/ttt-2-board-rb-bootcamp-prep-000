90380cfde0ce16e34e8bb7038a3553752fbf477e
Jason Porta <jasonmatthewporta@gmail.com>
2017-10-27 13:44:09 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,17 @@
 # Define the variable board below.
+
+=begin
+Define a local variable in lib/board.rb called board
+Set the board local variable equal to an array.
+Fill the board array with 9 strings containing a single space: " ".
+Run learn and read output and fix any errors.
+Submit your solution with learn submit.
+=end
+
+board = Array.new
+$i = 0
+until $i == 9 do
+  board.push(" ")
+  $i +=1
+end
+puts board.inspect
