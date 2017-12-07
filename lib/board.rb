b8ffbb674767b7438f0a2af31f86f3818ad1c848
Chaitanya Krishna Kanth Vankadaru <chaitanya.vankadaru@gmail.com>
2017-12-07 07:29:04 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
 # Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Position 1: Top Left
+board[0]
+
+# Position 2: Top Middle
+board[1]
+
+# Position 3: Top Right
+board[2]
+
+# Position 4: Middle Left
+board[3]
+
+# Position 5: Middle Middle
+board[4]
+
+# Position 6: Middle Right
+board[5]
+
+# Position 7: Bottom Left
+board[6]
+
+# Position 8: Bottom Middle
+board[7]
+
+# Position 9: Bottom Right
+board[8]
