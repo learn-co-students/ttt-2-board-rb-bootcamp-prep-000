a9727b7e0acf6f6046eaf7bc941a2ba579c93707
Margarita Timonina <margarita.timonina@gmail.com>
2018-01-14 19:59:48 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+#board
+board = [" ", " "," ", " ", " "," "," ", " "," "]
+board[1]
+board[2]
+board[3]
+board[4]
+board[5]
+board[6]
+board[7]
+board[8]
+board[9]
