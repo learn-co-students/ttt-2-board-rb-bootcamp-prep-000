355653d419be488a6a9efb2a72ec2fc6e154b4f5
Yolanda Galdamez <yolanda.v.galdamez@gmail.com>
2018-02-08 21:19:51 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,28 @@
 # Define the variable board below.
 board = [" "," "," "," "," "," "," "," "," "]
+# Position 1: Left
+board[0]
+ 
+# Position 2: Middle
+board[1]
+ 
+# Position 3: Right
+board[2]
+
+# Position 1: Left
+board[3]
+ 
+# Position 2: Middle
+board[4]
+ 
+# Position 3: Right
+board[5]
+
+# Position 1: Left
+board[6]
+ 
+# Position 2: Middle
+board[7]
+ 
+# Position 3: Right
+board[8]
