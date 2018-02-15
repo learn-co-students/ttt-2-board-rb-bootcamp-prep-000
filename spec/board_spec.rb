69cc2184e9aa81cda1d8fdb743a32b9209e5d975
Mary <l.skyling@gmail.com>
2018-02-15 15:49:30 +0000

    Done.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,3 @@
-var board;
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-
-
 describe "lib/board.rb" do
   it 'defines a local variable `board`' do
     board = get_variable_from_file("./lib/board.rb", "board")
