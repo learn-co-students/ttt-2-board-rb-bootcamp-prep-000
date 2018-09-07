daba4e8711fbcf71af21c23051e6806a27da58a5
jjtiojohn2012@gmail.com <jjtiojohn2012@gmail.com>
2018-09-07 17:43:26 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,6 @@
-row=["","",""]
-# position 1: left
-row[0]="x"
+board= [" "," "," "," "," "," "," "," "," "]
+  
 
-# position 2: Middle
-row[1]="x"
 
-# position 3: Right
+ 
 
-row[2]
-
-# move X to the position 2, Middle
-row[1]="X"
-row #=> ["","X",""]
-
-# move X to the position 3, Right
-row[2]="X"
-row #=> ["","","X"]
-
-# move x back to position 0, left
-row[0]="x"
-row #=> ["x","",""]
