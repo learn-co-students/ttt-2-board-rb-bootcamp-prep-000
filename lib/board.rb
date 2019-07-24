e03c239c75aed463535aedc6d8dacfa566be0a84
Tandi Samples <tandisamples@gmail.com>
2019-07-24 00:41:42 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,9 @@
-# Define the variable board below.
+# Define the variable board below
+puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+def display_board
+  puts row = ["   " "|" "   " "|" "   "]
+  puts separator = "-----------"
+  puts row
+  puts separator
+  puts row
+end
